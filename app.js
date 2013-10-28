3815a0c253da13ee53bf7a8f0401e35b29fb8464
gzmask <gzmask@gmail.com>
2013-10-28 06:15:06 +0000

    converted to static site

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
-var http = require('http'),
-    nodemailer = require("nodemailer"),
-    ss = require('socketstream');
+/* QUICK CHAT DEMO */
 
-// Define a single-page client called 'main'
-ss.client.define('main', {
-  view: 'index.html',
-  css:  ['libs/jquery.lightbox-0.5.css', 'libs/bootstrap.min.css', 'libs/bootstrap-responsive.min.css', 'libs/style.css'],
-  code: ['libs/jquery.min.js', 'app', 'libs/jquery.lightbox-0.5.min.js', 'libs/jquery.localscroll-1.2.7.js', 'libs/jquery.scrollTo-1.4.3.1.js', 'libs/bootstrap.min.js'],
-  tmpl: '*'
+// Delete this file once you've seen how the demo works
+
+// Listen out for newMessage events coming from the server
+ss.event.on('newMessage', function(message) {
+
+  // Example of using the Hogan Template in client/templates/chat/message.jade to generate HTML for each message
+  var html = ss.tmpl['chat-message'].render({
+    message: message,
+    time: function() { return timestamp(); }
+  });
+
+  // Append it to the #chatlog div and show effect
+  return $(html).hide().appendTo('#chatlog').slideDown();
 });
 
-// Serve this client on the root URL
-ss.http.route('/', function(req, res){
-  res.serveClient('main');
+// Show the chat form and bind to the submit action
+$('#demo').on('submit', function() {
+
+  // Grab the message from the text box
+  var text = $('#myMessage').val();
+
+  // Call the 'send' funtion (below) to ensure it's valid before sending to the server
+  return exports.send(text, function(success) {
+    if (success) {
+      return $('#myMessage').val('');
+    } else {
+      return alert('Oops! Unable to send message');
+    }
+  });
 });
 
-// Code Formatters
-//ss.client.formatters.add(require('ss-stylus'));
+// Demonstrates sharing code between modules by exporting function
+exports.send = function(text, cb) {
+  if (valid(text)) {
+    return ss.rpc('demo.sendMessage', text, cb);
+  } else {
+    return cb(false);
+  }
+};
 
-// Use server-side compiled Hogan (Mustache) templates. Others engines available
-//ss.client.templateEngine.use(require('ss-hogan'));
 
-// Minimize and pack assets if you type: SS_ENV=production node app.js
-if (ss.env === 'production') ss.client.packAssets();
+// Private functions
 
-// Start web server
-var server = http.Server(ss.http.middleware);
-server.listen(80);
+var timestamp = function() {
+  var d = new Date();
+  return d.getHours() + ':' + pad2(d.getMinutes()) + ':' + pad2(d.getSeconds());
+};
 
-// Start SocketStream
-ss.start(server);
+var pad2 = function(number) {
+  return (number < 10 ? '0' : '') + number;
+};
+
+var valid = function(text) {
+  return text && text.length > 0;
+};
